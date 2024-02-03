c4a2c9e267636d23ae0c69ecd4b8707e65c1d887
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 00:11:52 +0530

    Create inversioncount.java

---
+++ inversioncount.java
@@ -0,0 +1,20 @@
+//brute forec approach
+package Recursion; 
+
+public class numbers {
+    public static int inversion(int arr[], int n) {
+        int count=0;
+        for(int i=0;i<n-1;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(arr[i]>arr[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,1,3,5};
+        System.out.println(inversion(arr,arr.length));
+    }
+}
